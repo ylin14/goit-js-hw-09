5de7a10f46f04ac2a970b3b071d8667582efca9b
ylin14 <y.linyvyi@gmail.com>
2023-05-07 00:07:40 +0300

    reated color switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
+const refs = {
+    body: document.querySelector("body"),
+    startBtn: document.querySelector("[data-start]"),
+    stopBtn: document.querySelector("[data-stop]"),
+}
+
+refs.startBtn.addEventListener("click", () => {
+    if (refs.body.style.backgroundColor) {
+        return
+    } else { 
+        timerId = setInterval(() => {
+            refs.body.style.backgroundColor = getRandomHexColor()
+        }, 1000);;
+    }
+  });
+
+  refs.stopBtn.addEventListener("click", () => {
+    if (!refs.body.style.backgroundColor) {
+        return
+    } else {
+        clearInterval(timerId)
+        refs.body.style.backgroundColor = "";
+    }
+  })
