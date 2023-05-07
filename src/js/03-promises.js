c0ec62adcb583fe32ec9d4e3dc04440f90d3315a
ylin14 <y.linyvyi@gmail.com>
2023-05-07 23:29:07 +0300

    created promises generator

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,9 +2,6 @@ import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const refs = {
   formEl: document.querySelector('.form'),
-  inputDelayEl: document.querySelector('input[name="delay"]'),
-  inputStepEl: document.querySelector('input[name="step"]'),
-  inputAmountEl: document.querySelector('input[name="amount"]'),
 };
 
 function createPromise(position, delay) {
@@ -12,33 +9,28 @@ function createPromise(position, delay) {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       if (shouldResolve) {
-        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
       } else {
-        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
       }
-    });
+    }, delay);
   });
 
   promise.then(value => {
     Notify.success(value);
-  })
-    .catch(value => {
+  }).catch(value => {
       Notify.failure(value);
     });
 }
 
-
-let delay = refs.inputDelayEl.value;
-const step = refs.inputStepEl.value;
-const amount = refs.inputAmountEl.value;
-
 function onSubmitPromise(event) {
   event.preventDefault();
+  const form = new FormData(refs.formEl);
+  let delay = Number(form.get("delay"));
+  const step = Number(form.get("step"));
+  const amount = Number(form.get("amount"));
 
   for (let i = 0; i < amount; i += 1) {
-    console.log(amount);
-    console.log(delay);
-    console.log(step);
     createPromise(i + 1, delay);
     delay += step;
   }
